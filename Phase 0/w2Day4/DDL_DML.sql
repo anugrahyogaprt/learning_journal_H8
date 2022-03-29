205a439d3cdb9f86d08af6b497c3fe3396de3d06
Anugrah Yoga Pratama <retnoyoga76@yahoo.co.id>
2022-03-30 06:31:55 +0700

    add file till w3day2

--- Phase 0/w2Day4/DDL_DML.sql
+++ Phase 0/w2Day4/DDL_DML.sql
@@ -1,34 +1,35 @@
-CREATE TABLE teachers (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name varchar(25) NOT NULL,
-    last_name varchar(50),
-    school varchar(50) NOT NULL,
-    hire_date date,
-    salary numeric
-    )
-    
--- Menambahkan column Age
-ALTER TABLE teachers ADD age INT
+-- Create table teachers
+create table teachers(
+  id INT not NULL PRIMARY key AUTO_INCREMENT,
+  first_name VARCHAR(25),
+  last_name VARCHAR(50),
+  school VARCHAR(50),
+  hire_date DATE,
+  salary NUMERIC
+)
 
--- Menghapus column age
-ALTER TABLE teachers DROP COLUMN age
+-- Menambahkan column Age 
+ALTER TABLE teachers add age INT
 
--- Rename table teacher menjadi guru
-ALTER TABLE teachers RENAME TO guru
+-- Menghapus column age 
+alter table teachers drop column age
 
--- Ganti tipe data column salary
-ALTER TABLE guru MODIFY COLUMN salary INT
+-- Rename table teachers menjadi guru 
+alter table teachers rename to guru
 
--- ganti nama column first_name
-ALTER TABLE guru CHANGE COLUMN first_name nama_depan VARCHAR(25)
+-- Mengganti tipe data column salary 
+alter table guru modify column salary int
 
--- Ganti nama table ke teachers
-ALTER TABLE guru RENAME TO teachers
+-- Mengganti nama column "first_name" ke "nama_depan"
+alter table guru change COLUMN first_name nama_depan varchar(25)
 
--- ganti nama column "nama_depan"
-ALTER TABLE teachers CHANGE COLUMN nama_depan first_name VARCHAR(25)
+-- Mengganti nama table "guru" ke "teachers"
+alter table guru rename to teachers
 
--- Insert data ke database 1
+-- Mengganti nama column "nama_depan" ke "first_name"
+alter table teachers change COLUMN nama_depan first_name varchar(25)
+
+-- Insert data ke database 
 INSERT INTO teachers (id,first_name, last_name, school, hire_date, salary)
     VALUES (1,'Janet', 'Smith', 'MIT', '2011-10-30', 36200),
            (2,'Lee', 'Reynolds', 'MIT', '1993-05-22', 65000),
@@ -39,27 +40,76 @@ INSERT INTO teachers (id,first_name, last_name, school, hire_date, salary)
            (7,'James', 'Diaz', 'Harvard University', '2003-07-18', 61000),
            (8,'Zack', 'Smith', 'Harvard University', '2000-12-29', 55500),
            (9,'Luis', 'Gonzales', 'Standford University', '2002-12-01', 50000),
-           (10,'Frank', 'Abbers', 'Standford University', '1999-01-30', 66000)
-           
--- update salary si "Janet Smith"
-UPDATE teachers
-SET salary=50000
-WHERE first_name='Janet'
+           (10,'Frank', 'Abbers', 'Standford University', '1999-01-30', 66000);
 
--- Delete id=10
-DELETE FROM teachers
-WHERE id=10
+-- Update Salary "Janet Smith" 
+update teachers
+set salary=50000 
+where first_name='Janet'
 
--- Insert data ke 2 without id
+-- Delete id=10 
+delete from teachers 
+where id=10
+
+-- Insert Data 2 
 INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
     VALUES ('Samuel', 'Abbers', 'Standford University', '2006-01-30', 32000),
            ('Jessica', 'Abbers', 'Standford University', '2005-01-30', 33000),
            ('Tom', 'Massi', 'Harvard University', '1999-09-09', 39500),
            ('Esteban', 'Brown', 'MIT', '2007-01-30', 36000),
-           ('Carlos', 'Alonso', 'Standford University', '2001-01-30', 44000)
+           ('Carlos', 'Alonso', 'Standford University', '2001-01-30', 44000);
            
--- menghapus semua data
-TRUNCATE TABLE teachers
+-- Menghapus Semua Data 
+truncate table teachers
 
--- hapus table teachers
-DROP table teachers
+-- Menhapus table "teachers" 
+drop table teachers
+
+-- Get all data 
+select * from teachers
+
+-- Get id, firsta_name, last_name 
+select id, first_name, last_name 
+from teachers
+
+-- Membatasi jumlah output menjadi 4 
+select * 
+from teachers
+limit 0, 4
+
+-- Mengurutkan data berdasarkan column "first_name" dari Z-A
+select * 
+from teachers
+order by first_name DESC
+
+-- Mencari dosen yang mengajar di Harvard University 
+select *
+from teachers
+where school='Harvard University'
+
+-- Mencari dosen yang mengajar di Harvard University atau Stanford University (Cara 1)
+select *
+from teachers 
+where school='Harvard University' OR school='Standford University'
+
+-- Mencari dosen yang mengajar di Harvard University atau Stanford University (Cara 2)
+select *
+from teachers 
+where school in ('Harvard University', 'Standford University')
+
+-- Mencari unique universitas 
+select distinct(school)
+from teachers
+
+-- Mencari jumlah unique universitas 
+select count(distinct(school)) as jumlah_universitas
+from teachers 
+
+-- Melihat berapa banyak jumlah data 
+select count(*)
+from teachers
+
+-- Melihat data dosen bernama "james"
+select * 
+from teachers
+where BINARY first_name='james'
--- Phase 0/w2Day4/DQL.sql
+++ Phase 0/w2Day4/DQL.sql
@@ -1,42 +1,86 @@
--- Select all data
-SELECT * FROM teachers
-
--- Get id, first_name, last_name
-SELECT id, first_name, last_name
-FROM teachers
-
--- Membatasi jumlah output menjadi 4
-SELECT * 
-FROM teachers
-LIMIT 4, 5
-
--- Urutkan data berdasarkan column "first_name" from A-Z
-SELECT *
-FROM teachers
-ORDER BY first_name ASC
-
--- Conditional dosen yang mengajar di Harvard
-SELECT *
-FROM teachers
-WHERE school = 'Harvard University'
-
--- Mencari dosen yang mengajar di Harvard atau Stanford (Cara 1)
-SELECT *
-FROM teachers
-WHERE school='Harvard University' OR school = 'Standford University'
-
--- mencari lebih dari 1 (cara 2)
-SELECT *
-FROM teachers
-WHERE school IN ('Harvard University', 'Standford University')
-
--- Mencari  unique universitas
-SELECT DISTINCT (school)
-FROM teachers
-
--- Mencari jumlah unique universitas
-SELECT COUNT(DISTINCT (school)) as jumlah_universitas
-FROM teachers
 -- Melihat jumlah data 
-SELECT COUNT(*)
-FROM teachers
+select count(*)
+from teachers;
+
+-- Melihat total salary dosen 
+select sum(salary)
+from teachers;
+
+-- Melihat gaji dosen tertinggi yang mengajar di MIT 
+select id, first_name, last_name, max(salary)
+from teachers
+where school='MIT';
+
+-- Melihat rata-rata gaji dosen yang mengajar di Harvard University 
+select ceil(avg(salary))
+from teachers
+where school='Harvard University';
+
+-- Mencari dosen dengan first_name mengandung "sam" 
+select *
+from teachers
+where first_name like '%sam%';
+
+-- Mencari jumlah dosen disetiap universitas 
+select school, count(id) as total_dosen
+FROM teachers 
+group by school;
+
+-- Mencari rata-rata gaji disetiap universitas 
+select school, floor(AVG(salary))
+from teachers 
+group by school;
+
+-- Menampilkan guru dengan gaji tertinggi 
+select *
+from teachers
+where salary=(
+  select max(salary)
+  from teachers  
+ );
+
+-- Menampilkan setiap dosen yang memiliki gaji tertinggi disetiap universitas 
+select * 
+from teachers
+where (school, salary) in (
+  select school, max(salary)
+  from teachers
+  group by school
+ );
+
+-- Join table teachers dengan courses 
+select * 
+from teachers 
+join courses on teachers.id=courses.teachers_id
+order by teachers.id asc;
+
+-- Melihat dosen yang mengajar Calculus dengan gaji tertinggi (syntax yang salah)
+select teachers.id, teachers.first_name, teachers.Last_name, teachers.school, max(teachers.salary)
+from teachers 
+join courses on teachers.id=courses.teachers_id
+where courses.name='Calculus';
+
+-- Melihat dosen yang mengajar Physic dengan gaji terendah 
+select * 
+from teachers
+where salary=(
+  select min(salary)
+  from teachers 
+  join courses on teachers.id=courses.teachers_id
+  where courses.name='Physics'
+);
+
+-- Melihat dosen dengan jumlah mata kuliah yang diajarkan 
+select teachers.id, teachers.first_name, teachers.last_name, count(courses.id) as total_matkul
+from teachers 
+join courses on teachers.id=courses.teachers_id
+group by teachers.id
+order by total_matkul desc;
+
+-- Melihat dosen dengan jumlah mata kuliah yang diajarkan lebih dari 1
+select teachers.id, teachers.first_name, teachers.last_name, count(courses.id) as total_matkul
+from teachers 
+join courses on teachers.id=courses.teachers_id
+group by teachers.id
+having total_matkul>1
+order by total_matkul desc;
